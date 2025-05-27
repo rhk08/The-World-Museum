a4932044c56c754cd1bc5ce6d1f886c0d924c6b8
rhk08 <140364137+rhk08@users.noreply.github.com>
2025-05-28 00:13:28 +0930

    problem page elements layed out

--- randomshit/test.jsx
+++ randomshit/test.jsx
@@ -1,17 +1,16 @@
-
-      <div className='d'>
-        <div className='d-1'>
-          <p className='d-1-title font-inria-serif-bold font-size-64'>Objective 1.</p>
-          <p className='d-1-subtitle font-instrument-sans font-size-20'>INCREASE HANDLING EFFICIENCY</p>
+      <div className='e'>
+        <div className='e-1'>
+          <p className='e-1-title font-inria-serif-bold font-size-64'>Objective 4.</p>
+          <p className='e-1-subtitle font-instrument-sans font-size-20'>INCREASE HANDLING EFFICIENCY</p>
         </div>
 
-        <div className='d-image'><img src="images\white-gloves.png" alt="white gloves"/></div>
+        <div className='e-image'><img src="images\white-gloves.png" alt="white gloves"/></div>
 
-        <div className='d-2'>
-          <p className='d-2-title font-inria-serif-bold font-size-32'>What does this mean?</p>
-          <div className='d-2-line'></div>
-          <p className='d-2-text font-instrument-sans font-size-16'>This means the goal is to minimise the time spent handling objects, as increased handling time raises the risk of damage or loss. To achieve this, it's important to capture all necessary information in one efficient process. This includes location data, images, metadata, and details about the staff involved in handling the items.</p>
+        <div className='e-2'>
+          <p className='e-2-title font-inria-serif-bold font-size-32'>What does this mean?</p>
+          <div className='e-2-line'></div>
+          <p className='e-2-text font-instrument-sans font-size-16'>Enabling public access and engagement means using digital platforms to share the museum's collection with wider audiences through online catalogues, virtual exhibits, and interactive content. This increases public interest, supports academic research, and opens up opportunities for funding, partnerships, and revenue through greater visibility and community involvement.</p>
         </div>
 
-        <div className='d-3'></div>
+        <div className='e-3'></div>
       </div>
--- the-world-museum/src/views/ProblemPage.jsx
+++ the-world-museum/src/views/ProblemPage.jsx
@@ -18,19 +18,19 @@ const Problempage = () => {
   );
 
   return (
-    <main className="home" style={{height: 6000}} ref={container}>
+    <main className="home" style={{ height: 6000 }} ref={container}>
 
       {/* Component 1 */}
       <div className="a">
         <div className="a-1">
           <p className="a-1-title font-inria-serif-bold font-size-48">The Problem.</p>
 
-          <p className="a-1-subtext font-instrument-sans font-size-20">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+          <p className="a-1-subtext font-instrument-sans font-size-20">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
           </p>
-          
+
 
           <p className="a-1-subtitle font-instrument-sans font-size-20">So what did we do? <br></br>
-We started by.</p>
+            We started by.</p>
 
         </div>
 
@@ -39,7 +39,7 @@ We started by.</p>
         </div>
 
         <div className='a-2'>
-                    <p className="a-2-title font-inria-serif-bold font-size-48">Breaking it Down.</p>
+          <p className="a-2-title font-inria-serif-bold font-size-48">Breaking it Down.</p>
           <p className="a-2-subtext font-instrument-sans font-size-20">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
           </p>
         </div>
@@ -56,7 +56,7 @@ We started by.</p>
           <p className='b-1-subtitle font-instrument-sans font-size-20'>INCREASE HANDLING EFFICIENCY</p>
         </div>
 
-        <div className='b-image'><img src="images\white-gloves.png" alt="white gloves"/></div>
+        <div className='b-image'><img src="images\white-gloves.png" alt="white gloves" /></div>
 
         <div className='b-2'>
           <p className='b-2-title font-inria-serif-bold font-size-32'>What does this mean?</p>
@@ -71,11 +71,11 @@ We started by.</p>
       <div className='c'>
         <div className='c-1'>
           <p className='c-1-title font-inria-serif-bold font-size-64'>Objective 2.</p>
-          <p className='c-1-subtitle font-instrument-sans font-size-20'> 
+          <p className='c-1-subtitle font-instrument-sans font-size-20'>
             ACCURATE TRACKING OF ALL ITEMS TO PREVENT THEFT, LOSS, AND DAMAGE</p>
         </div>
 
-        <div className='c-image'><img src="images\paintings.png" alt="white gloves"/></div>
+        <div className='c-image'><img src="images\paintings.png" alt="white gloves" /></div>
 
         <div className='c-2'>
           <p className='c-2-title font-inria-serif-bold font-size-32'>What does this mean?</p>
@@ -86,23 +86,47 @@ We started by.</p>
         <div className='c-3'></div>
       </div>
 
-      
-      
+
+
       <div className='d'>
         <div className='d-1'>
           <p className='d-1-title font-inria-serif-bold font-size-64'>Objective 3.</p>
           <p className='d-1-subtitle font-instrument-sans font-size-20'>STANDARDISE PROCESS FOR MANAGEMENT</p>
         </div>
 
-        <div className='d-image'><img src="images\museum-stock.jpg" alt="white gloves"/></div>
+        <div className='d-image'><img src="images\museum-stock.jpg" alt="white gloves" /></div>
 
         <div className='d-2'>
           <p className='d-2-title font-inria-serif-bold font-size-32'>What does this mean?</p>
           <div className='d-2-line'></div>
-          <p className='d-2-text font-instrument-sans font-size-16'>Standardising the process for management involves creating a consistent and efficient workflow for key tasks such as imaging, data entry, conservation, and returning objects. By streamlining these processes, we can improve data accuracy, reduce errors, and ensure that all departments follow the same best practices when handling and documenting items.</p>
+          <p className='d-2-text font-instrument-sans font-size-16'>Standardising the process for management involves creating a consistent and efficient workflow for key tasks such as imaging, data entry, conservation, and returning objects. By streamlining these processes, we can improve data accuracy, reduce errors, and ensure that all departments follow the same best practices.</p>
         </div>
 
         <div className='d-3'></div>
+      </div>
+
+
+      <div className='e'>
+        <div className='e-1'>
+          <p className='e-1-title font-inria-serif-bold font-size-64'>Objective 4.</p>
+          <p className='e-1-subtitle font-instrument-sans font-size-20'>ENABLING AND PROMOTION OF PUBLIC ACCESS AND ENGAGEMENT</p>
+        </div>
+
+        <div className='e-image'><img src="images\clore-natural-history-centre.jpg" alt="white gloves" /></div>
+
+        <div className='e-2'>
+          <p className='e-2-title font-inria-serif-bold font-size-32'>What does this mean?</p>
+          <div className='e-2-line'></div>
+          <p className='e-2-text font-instrument-sans font-size-16'>Enabling public access and engagement means using digital platforms to share the museum's collection with wider audiences through online catalogues, virtual exhibits, and interactive content. This increases public interest, supports academic research, and opens up opportunities for funding, partnerships, and revenue through greater visibility and community involvement.</p>
+        </div>
+
+        <div className='e-3'></div>
+      </div>
+      
+
+      <div className='f'>
+
+        
       </div>
 
     </main>
